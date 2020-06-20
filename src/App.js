6b826e354c51d44100dfdb20ddddc582b2b041e1
Salman Azeem <salmanazeemkhan@gmail.com>
2020-06-20 16:02:17 +0400

    App code pusing

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Cards from './Components/Cards/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+    //console.log(data);
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    )
+  }
+}
+
+export default App;  
